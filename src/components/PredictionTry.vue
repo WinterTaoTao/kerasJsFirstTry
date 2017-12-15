798ac43c6d959e61da92089fe398dbbc76801a7e
Winter <xwhwt92@hotmail.com>
2017-12-15 17:02:41 +1000

    for loop, await, promise

--- src/components/PredictionTry.vue
+++ src/components/PredictionTry.vue
@@ -1,7 +1,9 @@
 <template>
-    <div>
-      {{msg}}
+  <div>
+    <div v-for="m in msg.split('\n')">
+      {{m}}<br>
     </div>
+  </div>
 </template>
 
 <script>
@@ -18,38 +20,57 @@
         msg: 'Predicting...'
       }
     },
-    created: function () {
+    // methods: {
+    //   forLoopPrediction: async function (num) {
+    //     for (let i = 1; i < num; i++) {
+    //       await this.model.ready()
+    //         .then(() => {
+    //           // const sample = [2]
+    //           const inputData = {
+    //             'input': new Float32Array([i])
+    //           }
+    //           // console.log(inputData)
+    //           return this.model.predict(inputData)
+    //         })
+    //         .then(outputData => {
+    //           console.log(i + ': ' + typeof (outputData.output[0]) + ' ' + outputData.output[0])
+    //           this.msg += '\n' + i + ': ' + outputData.output[0]
+    //         })
+    //     }
+    //   }
+    // },
+    created: async function () {
       this.model = new KerasJS.Model({
         filepath: MODEL_PATH + 'model.bin',
         filesystem: true
       })
-      // this.model = new KerasJS.Model({
-      //   filepaths: {
-      //     model: MODEL_PATH + 'model.json',
-      //     weights: MODEL_PATH + 'model_weights.buf',
-      //     metadata: MODEL_PATH + 'model_metadata.json'
-      //   },
-      //   filesystem: true
-      // })
-      for (let i = 1; i < 6; i++) {
-        this.model.ready()
-          .then(() => {
-            // const sample = [2]
-            const inputData = {
-              'input': new Float32Array([i])
-            }
-            // console.log(inputData)
-            return this.model.predict(inputData)
-          })
-          .then(outputData => {
-            console.log(typeof (outputData.output[0]))
-            this.msg += '\n' + i + ': ' + outputData.output[0]
-          })
-          // .catch(err => {
-          //   this.msg = 'Error: Can\'t Predict'
+      // this.forLoopPrediction(6)
+      for (let i = 0; i < 5; i++) {
+        await this.model.ready()
+        const inputData = {
+          input: new Float32Array([i])
+        }
+        console.log(i)
+        const outputData = await this.model.predict(inputData)
+        console.log(i, outputData.output[0])
+        this.msg += '\n' + i + ': ' + outputData.output[0]
+          // .then(() => {
+          //   // const sample = [2]
+          //   const inputData = {
+          //     'input': new Float32Array([i])
+          //   }
+          //   // console.log(inputData)
+          //   return this.model.predict(inputData)
           // })
+          // .then(outputData => {
+          //   console.log(i + ': ' + typeof (outputData.output[0]) + ' ' + outputData.output[0])
+          //   this.msg += '\n' + i + ': ' + outputData.output[0]
+          // })
+      //     .catch(err => {
+      //       this.msg = 'Error: Can\'t Predict'
+      //     })
       }
-
+      //
       // this.model.ready()
       //   .then(() => {
       //     // const sample = [2]
