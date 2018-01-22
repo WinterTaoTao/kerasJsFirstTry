f9139423a225cdd9b4e1b77c2181576c8d85b161
Winter <xwhwt92@hotmail.com>
2018-01-22 10:43:11 +1000

    start keyframes extractor

--- src/components/KeyframesExtractorTry.vue
+++ src/components/KeyframesExtractorTry.vue
@@ -3,8 +3,7 @@
 </template>
 
 <script>
-  const ffmpeg = require('ffmpeg')
-  // const fs = require('browserify-fs')
+  const ffmpeg = require('fluent-ffmpeg')
   export default {
     name: 'key-frame-etractor-try',
 
@@ -17,31 +16,8 @@
 
     mounted () {
       // eslint-disable-next-line
-      let process = new ffmpeg(this.sampleVideoPath)
-      console.log(process)
-      // process.screenshots({
-      //   count: 4,
-      //   folder: this.outputPath
-      // })
-      // console.log(process)
-      // try {
-      //   // eslint-disable-next-line
-      //   let process = new ffmpeg(this.sampleVideoPath)
-      //   console.log(process)
-      // } catch (e) {
-      //   console.log('Error:', e.code)
-      //   console.log(e.msg)
-      // }
-      // ffmpeg.ffmpeg(this.sampleVideoPath, {
-      //   width: 500,
-      //   height: 500
-      // }, this.output, function (progress) {
-      //   console.log(progress)
-      // }).success(function (json) {
-      //   console.log(json)
-      // }).error(function (error) {
-      //   console.log(error)
-      // })
+      const p = ffmpeg(this.sampleVideoPath)
+      console.log(p)
     }
   }
 </script>
